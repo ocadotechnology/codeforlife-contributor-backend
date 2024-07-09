44dfbb6691ac97d49107245512d9541230115e21
Salman Ashraf <salman.ashraf2513@gmail.com>
2024-07-09 12:57:27 +0000

    Check static code

--- api/models/agreement_signature_test.py
+++ api/models/agreement_signature_test.py
@@ -7,7 +7,7 @@ from codeforlife.tests import ModelTestCase
 
 from .agreement_signature import AgreementSignature
 from .contributor import Contributor
-from .repository import Repository
+# from .repository import Repository
 
 
 class TestAgreementSignature(ModelTestCase[AgreementSignature]):
@@ -19,7 +19,7 @@ class TestAgreementSignature(ModelTestCase[AgreementSignature]):
         self.agreement_signature = Contributor.objects.get(pk=1)
 
     # def test_str(self):
-    #     """Parsing a contributor object instance to string returns its name."""
+    #     """Parsing a contributor object instance to returns its name."""
     #     pass
 
     # def test_fields(self):
--- api/models/repository_test.py
+++ api/models/repository_test.py
@@ -5,8 +5,8 @@ Created on 09/07/2024 at 11:43:31(+01:00).
 
 from codeforlife.tests import ModelTestCase
 
-from .agreement_signature import AgreementSignature
-from .contributor import Contributor
+# from .agreement_signature import AgreementSignature
+# from .contributor import Contributor
 from .repository import Repository
 
 
@@ -19,7 +19,7 @@ class TestRepository(ModelTestCase[Repository]):
         self.repository = Repository.objects.get(pk=111)
 
     # def test_str(self):
-    #     """Parsing a contributor object instance to string returns its name."""
+    #     """Parsing a contributor object instance to returns its name."""
     #     assert str(self.repository) == self.repository.name
 
     # def test_fields(self):
