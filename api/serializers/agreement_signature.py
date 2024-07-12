c040e515b7c0484e2bda040eb7ec80b6e072ec00
SalmanAsh <salman.ashraf2513@gmail.com>
2024-07-12 13:08:42 +0000

    Add module doc strings

--- api/serializers/agreement_signature.py
+++ api/serializers/agreement_signature.py
@@ -1,3 +1,8 @@
+"""
+© Ocado Group
+Created on 12/07/2024 at 14:07:45(+01:00).
+"""
+
 from codeforlife.serializers import ModelSerializer
 from codeforlife.user.models import User
 
--- api/serializers/contributor.py
+++ api/serializers/contributor.py
@@ -1,3 +1,7 @@
+"""
+© Ocado Group
+Created on 12/07/2024 at 14:07:59(+01:00).
+"""
 from codeforlife.serializers import ModelSerializer
 from codeforlife.user.models import User
 
