3f3b81e3f6bc6b81e2041060a2e61aada1780d7f
SalmanAsh <salman.ashraf2513@gmail.com>
2024-07-17 12:55:51 +0000

    Add validation on time of signature

--- api/serializers/agreement_signature.py
+++ api/serializers/agreement_signature.py
@@ -3,10 +3,16 @@
 Created on 12/07/2024 at 14:07:45(+01:00).
 """
 
+from datetime import datetime
+
 from codeforlife.serializers import ModelSerializer
 from codeforlife.user.models import User
+from django.utils import timezone
+from rest_framework import serializers
 
 from ..models import AgreementSignature
+
+# pylint: disable-next=missing-function-docstring,too-many-ancestors
 
 
 class AgreementSignatureSerializer(ModelSerializer[User, AgreementSignature]):
@@ -15,3 +21,11 @@ class AgreementSignatureSerializer(ModelSerializer[User, AgreementSignature]):
     class Meta:
         model = AgreementSignature
         fields = ["id", "contributor", "agreement_id", "signed_at"]
+
+    def validate_signed_at(self, value: datetime):
+        """Validate the time in not in future."""
+        if value > timezone.now():
+            raise serializers.ValidationError(
+                "Cannot sign in the future", code="signed_in_future"
+            )
+        return value
--- api/serializers/agreement_signature_test.py
+++ api/serializers/agreement_signature_test.py
@@ -3,16 +3,29 @@
 Created on 12/07/2024 at 17:16:58(+01:00).
 """
 
+from datetime import timedelta
+
 from codeforlife.tests import ModelSerializerTestCase
 from codeforlife.user.models import User
+from django.utils import timezone
 
 from ..models import AgreementSignature
 from .agreement_signature import AgreementSignatureSerializer
 
 
+# pylint: disable-next=missing-function-docstring,too-many-ancestors
 class TestAgreementSignatureSerializer(
     ModelSerializerTestCase[User, AgreementSignature]
 ):
     """Test the Agreement Signature serializers"""
 
     model_serializer_class = AgreementSignatureSerializer
+
+    def test_validate_signed_at__signed_in_future(self):
+        """Can validate the time the signature was signed at."""
+        time = timezone.now() + timedelta(hours=10)
+        self.assert_validate_field(
+            name="signed_at",
+            value=time,
+            error_code="signed_in_future",
+        )
