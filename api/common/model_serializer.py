8b18a944aa0fa176a743b670680f496cf92ca6c9
SKairinos <stefan.kairinos@ocado.com>
2024-11-06 12:22:16 +0000

    use base model serializer and view set

--- api/common/model_serializer.py
+++ api/common/model_serializer.py
@@ -6,9 +6,8 @@ Created on 13/09/2024 at 12:00:41(+03:00).
 import typing as t
 
 from codeforlife.request import BaseRequest
-from codeforlife.types import DataDict
+from codeforlife.serializers import BaseModelSerializer
 from django.db.models import Model
-from rest_framework.serializers import ModelSerializer as _ModelSerializer
 
 from ..models import Contributor
 from ..models.session import SessionStore
@@ -16,42 +15,8 @@ from ..models.session import SessionStore
 AnyModel = t.TypeVar("AnyModel", bound=Model)
 
 
-class ModelSerializer(_ModelSerializer[AnyModel], t.Generic[AnyModel]):
+class ModelSerializer(
+    BaseModelSerializer[BaseRequest[SessionStore, Contributor], AnyModel],
+    t.Generic[AnyModel],
+):
     """Base model serializer."""
-
-    @property
-    def request(self):
-        """The HTTP request that triggered the view."""
-
-        return t.cast(
-            BaseRequest[SessionStore, Contributor], self.context["request"]
-        )
-
-    @property
-    def view(self):
-        """The view that instantiated this serializer."""
-        # NOTE: import outside top-level to avoid circular imports.
-        # pylint: disable-next=import-outside-toplevel
-        from .model_view_set import ModelViewSet
-
-        return t.cast(ModelViewSet[AnyModel], self.context["view"])
-
-    @property
-    def non_none_instance(self):
-        """Casts the instance to not None."""
-        return t.cast(AnyModel, self.instance)
-
-    # pylint: disable-next=useless-parent-delegation
-    def update(self, instance: AnyModel, validated_data: DataDict) -> AnyModel:
-        return super().update(instance, validated_data)
-
-    # pylint: disable-next=useless-parent-delegation
-    def create(self, validated_data: DataDict) -> AnyModel:
-        return super().create(validated_data)
-
-    def validate(self, attrs: DataDict):
-        return attrs
-
-    # pylint: disable-next=useless-parent-delegation
-    def to_representation(self, instance: AnyModel) -> DataDict:
-        return super().to_representation(instance)
--- api/common/model_view_set.py
+++ api/common/model_view_set.py
@@ -6,8 +6,8 @@ Created on 13/09/2024 at 12:00:57(+03:00).
 import typing as t
 
 from codeforlife.request import BaseRequest
+from codeforlife.views import BaseModelViewSet
 from django.db.models import Model
-from rest_framework.viewsets import ModelViewSet as _ModelViewSet
 
 from ..models import Contributor
 from ..models.session import SessionStore
@@ -17,32 +17,13 @@ AnyModel = t.TypeVar("AnyModel", bound=Model)
 if t.TYPE_CHECKING:  # pragma: no cover
     from .model_serializer import ModelSerializer
 
-    # NOTE: This raises an error during runtime.
-    # pylint: disable-next=too-few-public-methods,invalid-name
-    class __ModelViewSet(_ModelViewSet[AnyModel], t.Generic[AnyModel]):
-        pass
-
-else:
-    # pylint: disable-next=too-many-ancestors,invalid-name
-    class __ModelViewSet(_ModelViewSet, t.Generic[AnyModel]):
-        pass
-
 
 # pylint: disable-next=too-many-ancestors
-class ModelViewSet(__ModelViewSet[AnyModel], t.Generic[AnyModel]):
+class ModelViewSet(
+    BaseModelViewSet[BaseRequest[SessionStore, Contributor], AnyModel],
+    t.Generic[AnyModel],
+):
     """Base model view set."""
 
-    request: BaseRequest[SessionStore, Contributor]
+    request_class = BaseRequest[SessionStore, Contributor]
     serializer_class: t.Optional[t.Type["ModelSerializer[AnyModel]"]]
-
-    def initialize_request(self, request, *args, **kwargs):
-        # NOTE: Call to super has side effects and is required.
-        super().initialize_request(request, *args, **kwargs)
-
-        return BaseRequest[SessionStore, Contributor](
-            request=request,
-            parsers=self.get_parsers(),
-            authenticators=self.get_authenticators(),
-            negotiator=self.get_content_negotiator(),
-            parser_context=self.get_parser_context(request),
-        )
