0a06726996f13b54e7961b376a7dbd0479e5e21c
SKairinos <stefan.kairinos@ocado.com>
2024-11-15 12:49:02 +0000

    new cfl package

---
+++ api/management/commands/load_fixtures.py
@@ -0,0 +1,7 @@
+"""
+© Ocado Group
+Created on 15/11/2024 at 12:26:15(+00:00).
+"""
+
+# pylint: disable-next=unused-import
+from codeforlife.commands import LoadFixtures as Command
---
+++ api/management/commands/summarize_fixtures.py
@@ -0,0 +1,7 @@
+"""
+© Ocado Group
+Created on 15/11/2024 at 12:26:21(+00:00).
+"""
+
+# pylint: disable-next=unused-import
+from codeforlife.commands import SummarizeFixtures as Command
