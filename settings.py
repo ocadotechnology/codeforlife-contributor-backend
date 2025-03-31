9b36270b8eed2c5fbcca12b8cdf2a9d2a1ea6f2a
SKairinos <stefan.kairinos@ocado.com>
2025-03-31 17:22:10 +0000

    remove unnecessary import

--- settings.py
+++ settings.py
@@ -15,7 +15,6 @@ import os
 from pathlib import Path
 
 from codeforlife import set_up_settings
-from codeforlife.tasks import CeleryBeat
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent
